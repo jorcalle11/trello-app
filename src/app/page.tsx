2b35ef1a8ad14b16cd8457565602f5925aed3d0b
Jorge Calle <jorcalle11@gmail.com>
2024-12-10 23:41:13 -0500

    Update CTA button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
       <p className="mb-4">Welcome to Trello App</p>
       {/* Creates a CTA button to redirect the user to the login page */}
       <Link
-        href="/login"
+        href="/boards"
         className="px-6 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700"
       >
-        Login
+        Boards
       </Link>
     </main>
   );
