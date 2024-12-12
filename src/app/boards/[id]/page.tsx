32ebc68cdd675fa3ab6897906607b7636e14c5ed
Jorge Calle <jorcalle11@gmail.com>
2024-12-11 20:36:07 -0500

    Add icon in the back link

--- src/app/boards/[id]/page.tsx
+++ src/app/boards/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { fetchBoardById } from "@/app/lib/data";
 import { Board } from "@/app/ui/Board";
 import Link from "next/link";
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 
 export default async function BoardDetailPage(props: {
   params: Promise<{ id: string }>;
@@ -15,11 +16,11 @@ export default async function BoardDetailPage(props: {
       style={{ backgroundColor: board.color }}
     >
       <div className="mb-8">
-        {/* wrap the h1 in a div to show a back button in the right to redirect the user to the /boards page */}
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-3xl font-bold">{board.title}</h1>
-          <Link href="/boards" className="text-white underline">
-            Back
+          <Link href="/boards" className="text-white flex">
+            <ArrowLeftIcon className="h-6 w-6" />
+            <span className="ml-2">Back to boards</span>
           </Link>
         </div>
       </div>
